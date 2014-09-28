79d5f616fea24514e90daf7684e7d7ee53d04683
Volodymyr Lysiuk <karapuzzx@gmail.com>
2014-09-28 11:55:25 +0300

    added data entities

---
+++ DontForget/Data/DontForgetList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace DontForget.Data
+{
+    public class DontForgetList
+    {
+        public DontForgetList(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public IList<string> Items { get; private set; }
+    }
+}
