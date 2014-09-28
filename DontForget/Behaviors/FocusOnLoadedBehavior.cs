6c9f21d7bb4e9c8cb4b6ffca8368a41d0561c212
Volodymyr Lysiuk <karapuzzx@gmail.com>
2014-09-28 23:36:40 +0300

    added focus on loaded behavior

---
+++ DontForget/Behaviors/FocusOnLoadedBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace DontForget.Behaviors
+{
+    public class FocusOnLoadedBehavior : Behavior<Control>
+    {
+        public static readonly DependencyProperty SetFocusOnLoadedProperty =
+            DependencyProperty.Register(
+                "SetFocusOnLoaded",
+                typeof(bool),
+                typeof(FocusOnLoadedBehavior),
+                new PropertyMetadata(true));
+
+        public bool SetFocusOnLoaded
+        {
+            get { return (bool) GetValue(SetFocusOnLoadedProperty); }
+            set { SetValue(SetFocusOnLoadedProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.Loaded += ObjectLoaded;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            AssociatedObject.Loaded -= ObjectLoaded;
+        }
+
+        private void ObjectLoaded(object sender, RoutedEventArgs e)
+        {
+            AssociatedObject.Loaded -= ObjectLoaded;
+
+            if (SetFocusOnLoaded)
+            {
+                AssociatedObject.Focus();
+            }
+        }
+    }
+}
